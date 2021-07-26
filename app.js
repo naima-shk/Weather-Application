180bd17359538f36a2a1383b51bafe4c4d4f59e2
naima-shk <naimashaikh550@gmail.com>
2021-07-26 14:00:18 +0500

    'implement-weather-application'

--- app.js
+++ app.js
@@ -29,12 +29,13 @@ function displayResults(weather){
     let now = new Date()
     let date = document.querySelector('.location .date');
     date.innerText=dateBuilder(now);
+
     let temp = document.querySelector('.current .temp');
-    temp.innerHTML= `${Math.round(weather.main.temp)}<span>C</span>`
+    temp.innerHTML= `${Math.round(weather.main.temp).toFixed(0)}<span>C</span>`
     let weather_el =document.querySelector('.current .weather');
     weather_el.innerText=weather.weather[0].main;
     let hilow =document.querySelector('.hi-low');
-    hilow.innerText= `${weather.main.temp_min} C / ${weather.main.temp_max} C`;
+    hilow.innerText= `${Math.round(weather.main.temp_min)} C / ${Math.round(weather.main.temp_max)} C`;
 }
 
 function dateBuilder(d){
