80542f401bb32b9fa2d87d5f4bf73bf00c7628c0
naima-shk <naimashaikh550@gmail.com>
2021-07-29 17:05:06 +0500

    'implement-weather-application'

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const api ={
     base: 'https://api.openweathermap.org/data/2.5/'
 }
 //set up event listener on the search box
-const searchBox= document.querySelector('.search-box');
-searchBox.addEventListener('keypress', setQuery);
+const searchbox= document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
 //when we press a key and then the key were actually looking to be pressed it's number 13 which is equal to the enter key or the return key on your keyboard and this will over ride this
 function setQuery(evt){
     if(evt.keyCode ==13){
-        getResults(searchBox.value);
-        //console.log(searchBox.value)
+        getResults(searchbox.value);
+        console.log(searchbox.value)
     }
 }
 //then it will run a fetch request which is going to say .we are gonna get the API dot base of this. we are gonna attach the weather at the end we are then gonna pass through a query which is got from our search box
@@ -19,17 +19,17 @@ function getResults(query){
   .then(weather =>{
       return weather.json();
   }).then(displayResults);
-
+​
 }
 function displayResults(weather){
     //console.log(weather);
     let city =document.querySelector('.location .city');
-    city.innerText = `${weather.name} ${weather.sys.country}`;
-
-    let now = new Date()
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+​
+    let now = new Date();
     let date = document.querySelector('.location .date');
     date.innerText=dateBuilder(now);
-
+​
     let temp = document.querySelector('.current .temp');
     temp.innerHTML= `${Math.round(weather.main.temp)}<span>C</span>`
     let weather_el =document.querySelector('.current .weather');
@@ -37,7 +37,7 @@ function displayResults(weather){
     let hilow =document.querySelector('.hi-low');
     hilow.innerText= `${Math.round(weather.main.temp_min)} C / ${Math.round(weather.main.temp_max)} C`;
 }
-
+​
 function dateBuilder(d){
     let months =['january', 'feb', 'march','april', 'june', 'july','august', 'september', 'october', 'november', 'december'];
     let days= ['monday', 'tuesday', 'wednesday', 'thursday','friday', 'saturday', 'sunday'];
@@ -45,6 +45,6 @@ function dateBuilder(d){
      let date = d.getDate();
      let month= months[d.getMonth()];
      let year =d.getFullYear();
-
+​
      return `${day} ${date} ${month} ${year}`;
 }
